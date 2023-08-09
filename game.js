e23c4f927b6d003c28cf8948a29509ad5eaaccd1
arieldelg <136664352+arieldelg@users.noreply.github.com>
2023-08-08 18:11:09 -0700

    fase 2

--- game.js
+++ game.js
@@ -1,21 +1,40 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-
+let canvasSize;
+let elementsSize;
 
 const startGame = () => {
-      let canvasSize;
+    console.log({elementsSize, canvasSize})
 
-    if(window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
-    } else {
-        canvasSize = window.innerHeight * 0.8;
-    }
+    game.font = elementsSize + 'px Verdana'
+    game.textAlign = 'end';
 
+    /* crea row y columnas de los mapas */
+    
 
-    canvas.setAttribute('width', canvasSize);
-    canvas.setAttribute('heigth', canvasSize);  
-    window.innerWidth
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            game.fillText(emojis['X'], ((elementsSize + 1) * [j]) + 6 ,(elementsSize) * [i]) 
+        }
+    } 
+    
 }
 
-window.addEventListener('load', startGame);
+const renderCanvas = () => {
+    if(window.innerHeight > window.innerWidth) {
+        canvasSize = innerWidth * .4608296
+    } else {
+        canvasSize = innerHeight * .4608296
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize); 
+
+    elementsSize = (canvasSize / 10) -1
+
+    startGame()
+}
+
+window.addEventListener('resize', renderCanvas)
+window.addEventListener('load', renderCanvas);
