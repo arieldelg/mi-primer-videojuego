6abad9d637973e4cb35251498d60dcc765c61d72
arieldelg <136664352+arieldelg@users.noreply.github.com>
2023-08-11 13:10:40 -0700

    fase final

--- game.js
+++ game.js
@@ -4,11 +4,21 @@ const up = document.getElementById('up');
 const left = document.getElementById('left');
 const right = document.getElementById('right');
 const down = document.getElementById('down');
+const spanLifes = document.getElementById('vidas');
+const spanTime = document.getElementById('time');
+const record = document.getElementById('record')
+const pResult = document.getElementById('result')
 
 let canvasSize;
 let elementsSize;
 /* let positionDoorX; parte de la funcion 1
 let positionDoorY; */
+let level = 0;
+let positionBombas = []
+let lifes = 3;
+let timestart;
+let setIntervalGame;
+let clearIntervalGame;
 
 const positionPlayer = {
     x: undefined,
@@ -19,9 +29,6 @@ const ganaste = {
     x: undefined,
     y: undefined
 }
-
-const positionBombas = []
-
 
 const renderCanvas = () => {
     if(window.innerHeight > window.innerWidth) {
@@ -34,30 +41,44 @@ const renderCanvas = () => {
     canvas.setAttribute('height', canvasSize); 
 
     elementsSize = Math.floor(canvasSize / 10) 
-
+    positionPlayer.x = undefined
+    positionPlayer.y = undefined
     startGame()
 }
 
 const startGame = () => {
     console.log({elementsSize, canvasSize})
-
+    positionBombas = []
     game.font = elementsSize + 'px Verdana'
     game.textAlign = '';
 
-    const mapas = maps[0].trim().split('\n')
-    const mapRows = mapas.map(element => element.trim())
+    let mapas = maps[level]
+    
+    if (!mapas) {
+        return gameWin()
+    }
+
+    if (!timestart) {
+        timestart = Date.now()
+        setIntervalGame = setInterval(showTime, 100)
+        recordTime()
+    }
+
+    mostrarVidas()
+
+    const newMaps = mapas.trim().split('\n')
+    const mapRows = newMaps.map(element => element.trim())
     //console.log({mapas, mapRows})
     const mapColumn = mapRows.map(e => e.split(''))
     //console.log({mapColumn})
     //con un ciclo forEach
     game.clearRect(0,0, canvasSize, canvasSize)
+    
     mapColumn.forEach((i, iIndex) => {
         i.forEach((j, jIndex) => {
             const emoji = emojis[j]
             const positionX = ((elementsSize ) * jIndex);
             const positionY = ((elementsSize) * (iIndex + 1));
-            
-           
             
             if(j === 'O') {
                 if(!positionPlayer.x && !positionPlayer.y) {
@@ -70,7 +91,7 @@ const startGame = () => {
                 ganaste.x = positionX;
                 ganaste.y = positionY 
             } else if (j === 'X') {
-                if(positionBombas.length !== positionBombas.length) {
+                if(positionBombas) {
                 positionBombas.push({
                     x: positionX,
                     y: positionY
@@ -109,7 +130,6 @@ const mouseUp = () => {
     //drawDoor() parte de la funcion 1
 }
 const mouseDown = () => {
-    console.log(positionPlayer.x, positionPlayer.y);
     if (canvasSize === 195) {
         if(positionPlayer.y < canvasSize - elementsSize) {
             positionPlayer.y += elementsSize
@@ -121,7 +141,6 @@ const mouseDown = () => {
    }
 }
 const mouseRight = () => {
-    console.log(positionPlayer.x , positionPlayer.y)
     if(canvasSize === 195) {
         if(positionPlayer.x < canvasSize - (elementsSize * 2)) {
             positionPlayer.x += elementsSize
@@ -136,8 +155,7 @@ const mouseLeft = () => {
     if(positionPlayer.x !== 0) {
         positionPlayer.x -= elementsSize
         startGame()
-    }
-    
+    } 
 }
 
 window.addEventListener('keydown', pressKey);
@@ -153,10 +171,87 @@ window.addEventListener('load', renderCanvas);
 
 const movePlayer = () => {
     if (ganaste.x === positionPlayer.x && ganaste.y === positionPlayer.y) {
-        console.log('ganaste')
+        levelUp ()
     }
-    game.fillText(emojis['PLAYER'], positionPlayer.x, positionPlayer.y)
+
+    /* const checarColisionBomba = positionBombas.find(element => {  es una manera de checar collision con las bombas con metodo to find
+        const bombasX = element.x.toFixed(3) === positionPlayer.x.toFixed(3);
+        const bombasY = element.y.toFixed(3) === positionPlayer.y.toFixed(3);
+        return bombasX && bombasY;
+    })
+
+    if (checarColisionBomba) {
+        console.log('perdiste')
+    } */
+     checarColisionBomba() 
+    game.fillText(emojis['PLAYER'], positionPlayer.x, positionPlayer.y);
+}
+
+const checarColisionBomba = () => {
+    const bomba = positionBombas.find(element => {
+        const collissionX = element.x == positionPlayer.x;
+        const collisionY = element.y == positionPlayer.y
+        return collissionX && collisionY
+    })
+    if(bomba) {
+        //console.log('perdiste');
+        gameEnd()
+    }
+} 
+
+const levelUp = () => {
+    level++
+    startGame()
+}
+
+const gameWin = () => {
+    clearInterval(setIntervalGame);
+
+    const record = localStorage.getItem('recordTime');
+    const playerTime = Date.now() - timestart;
+
+    if (record) {
+        if (playerTime <= record) {
+            pResult.innerHTML = 'superaste el record';
+            localStorage.setItem('recordTime', playerTime);
+        } else {
+            pResult.innerHTML = 'No superaste el record';
+        }
+    } else {
+        localStorage.setItem('recordTime', playerTime);
+    }
+    console.log({record, playerTime})
     
+    window.location.reload()
+}
+
+const gameEnd = () => {
+    lifes--
+
+    if(lifes <= 0) {
+        lifes = 3
+        level = 0
+        clearInterval(setIntervalGame)
+        timestart = undefined
+    }
+    positionPlayer.x = undefined
+    positionPlayer.y = undefined
+    
+    /* window.location.reload() */
+}
+
+const mostrarVidas = () => {
+    const lifesArray = Array(lifes).fill(emojis['HEART'])
+    spanLifes.innerHTML = ''
+    lifesArray.forEach(element => spanLifes.innerHTML += element)
+}
+
+const showTime = () => {
+ spanTime.innerHTML = Date.now() - timestart
+}
+
+const recordTime = () => {
+    record.innerHTML = localStorage.getItem('recordTime');
 }
 
 /* 
